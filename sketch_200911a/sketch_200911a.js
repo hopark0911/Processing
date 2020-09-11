6f97679bec04bc45e6b88463fc1f852cbf5a16dc
hopark0911 <70361035+hopark0911@users.noreply.github.com>
2020-09-11 14:59:11 +0900

    ㅎㅎㅎ

---
+++ sketch_200911a/sketch_200911a.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(500, 500);
+  fill(356, 25, 22)
+  ellipse(100, 33, 50, 50 );
+  fill(45, 56, 35)
+  ellipse(10, 20, 40, 40 );
+  fill(28, 24, 15);
+  ellipse(200, 70, 50, 50 );
+  fill(255, 234, 0);
+  ellipse(250,45, 50, 50 );
+  fill(30, 30, 0);
+  ellipse(400, 255, 10, 10 );
+}
+
+function draw() {
+
+}
